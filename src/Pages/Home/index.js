cf1343355763fcc4ad6b5ac777ff3210e2e80ef8
Ademilson Bertolin <ademilsonbertolinnn@gmail.com>
2023-03-28 22:35:40 -0300

    Detalhes nos filmes

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,32 +1,60 @@
-import { useEffect, useState } from "react";
-import api from "../../services/api";
+import { useEffect, useState} from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
 
-//URL: https://api.themoviedb.org/3/movie/now_playing?api_key=d330c193d76504d0b0c0a8780235060c&language=pt-BR
+// URL DA API: /movie/now_playing?api_key=28fc232cc001c31e8a031f419d0a14ca&language=pt-BR
 
 function Home(){
-
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
+
+  useEffect(()=>{
 
     async function loadFilmes(){
       const response = await api.get("movie/now_playing", {
         params:{
-          api_key: "d330c193d76504d0b0c0a8780235060c",
-          language: "pt-BR",
-          page: 1,
+         api_key: "28fc232cc001c31e8a031f419d0a14ca",
+         language: "pt-BR",
+         page: 1,
         }
+      
       })
-      console.log(response);
+      // console.log(response);
+      //console.log(response.data.results.slice(0, 10));
+      setFilmes(response.data.results.slice(0, 10))
+      setLoading(false);
+
     }
 
     loadFilmes();
 
-  },[])
+  }, [])
+
+
+
+  if(loading){
+    return(
+      <div className="loading">
+        <h2>Carregando filmes...</h2>
+      </div>
+    )
+  }
 
   return(
-    <div>
-      <h1>BEM VINDO A HOME</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
     </div>
   )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
 
-//base da URL: https://api.themoviedb.org/3/
-//URL: https://api.themoviedb.org/3/movie/now_playing?api_key=d330c193d76504d0b0c0a8780235060c&language=pt-BR
+// Base da URL: https://api.themoviedb.org/3/
+//URL DA API: /movie/now_playing?api_key=28fc232cc001c31e8a031f419d0a14ca&language=pt-BR
 
 const api = axios.create({
-    baseURL: 'https://api.themoviedb.org/3/'
+  baseURL: 'https://api.themoviedb.org/3/'
 });
 
 export default api;
